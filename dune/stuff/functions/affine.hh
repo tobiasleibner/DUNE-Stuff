3124e322519495aae674f2dfcfe5597a99582163
Tobias Leibner <tobias.leibner@googlemail.com>
2015-07-07 12:22:53 +0200

    [functions.affine] remove references in the constructor

--- dune/stuff/functions/affine.hh
+++ dune/stuff/functions/affine.hh
@@ -82,7 +82,7 @@ public:
           cfg.get("name",  default_cfg.get< std::string >("name")));
   } // ... create(...)
 
-  explicit Affine(const MatrixType& matrix, const RangeType& vector = RangeType(0), const bool sparse = false, const std::string name_in = static_id())
+  explicit Affine(const MatrixType matrix, const RangeType vector = RangeType(0), const bool sparse = false, const std::string name_in = static_id())
     : A_(matrix)
     , b_(vector)
     , name_(name_in)
