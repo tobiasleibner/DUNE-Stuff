b575f2d7b05925fdd8fefe34455fe97e9aeea3c6
Tobias Leibner <tobias.leibner@uni-muenster.de>
2016-04-25 11:10:11 +0200

    [grid.walker] always walk parallel

--- dune/stuff/grid/walker.hh
+++ dune/stuff/grid/walker.hh
@@ -16,10 +16,10 @@
 
 #include <dune/common/version.hh>
 
-#if DUNE_VERSION_NEWER(DUNE_COMMON,3,9) //EXADUNE
-# include <dune/grid/utility/partitioning/ranged.hh>
+//#if DUNE_VERSION_NEWER(DUNE_COMMON,3,9) //EXADUNE
+# include <dune/grid/utility/ranged.hh>
 # include <dune/stuff/common/parallel/threadmanager.hh>
-#endif
+//#endif
 
 #if HAVE_TBB
 # include <tbb/blocked_range.h>
@@ -191,7 +191,7 @@ public:
 
   void walk(const bool use_tbb = false)
   {
-#if DUNE_VERSION_NEWER(DUNE_COMMON,3,9) //EXADUNE
+//#if DUNE_VERSION_NEWER(DUNE_COMMON,3,9) //EXADUNE
     if (use_tbb) {
       const auto num_partitions = DSC_CONFIG_GET("threading.partition_factor", 1u)
                                   * threadManager().current_threads();
@@ -199,9 +199,9 @@ public:
       this->walk(partitioning);
       return;
     }
-#else
+//#else
     const auto DUNE_UNUSED(no_warning_for_use_tbb) = use_tbb;
-#endif
+//#endif
     // prepare functors
     prepare();
 
