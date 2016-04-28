bd9dfb8b49b0c27ce0326ccdd563673356f3c6ae
Tobias Leibner <tobias.leibner@uni-muenster.de>
2016-04-28 10:33:46 +0200

    [common.parallel.threadmanager] disable assert

--- dune/stuff/common/parallel/threadmanager.cc
+++ dune/stuff/common/parallel/threadmanager.cc
@@ -28,7 +28,6 @@
 size_t Dune::Stuff::ThreadManager::max_threads()
 {
   const auto threads = DSC_CONFIG_GET("threading.max_count", 1);
-  WITH_DUNE_FEM(assert(Dune::Fem::ThreadManager::maxThreads() == threads);)
 //  WITH_DUNE_FEM(assert(Dune::Fem::ThreadManager::maxThreads() == threads);)
   return threads;
 }
@@ -37,7 +36,6 @@ size_t Dune::Stuff::ThreadManager::current_threads()
 {
   const auto threads = max_threads();
 //  WITH_DUNE_FEM(assert(long(Dune::Fem::ThreadManager::currentThreads()) == long(threads));)
-  WITH_DUNE_FEM(assert(long(Dune::Fem::ThreadManager::currentThreads()) == long(threads));)
   return threads;
 }
 
